b14df6690979da10b095937acef9f47d4f29c743
Jeriesha Smith <jerieshasmith@jerieshas-mbp.nvstu.edu>
2017-04-07 08:59:19 -0400

    First commit

---
+++ PetBit/src/PetBit.java
@@ -0,0 +1,21 @@
+/**
+ * Created by jerieshasmith on 4/6/17.
+ */
+public class PetBit {
+
+    PetBit
+    Pet bit will be an interactive device that can be controlled on any device. As humans, we are truly concerned about our health and our daily livelihoods. Pet lovers are just as concerned about their pets as they treat them as children. The one thing as a pet lover that we cannot identify is whether our pets are in good health, because they cannot tell us when they are not feeling well. PETBIT will do the following;
+-	It will allow you to enter your pet breed, and automatically download acceptable health conditions for your pet.
+            -	It will monitor resting heart rate for your breed, and give you alerts for irregularities during resting periods.
+            -	It will monitor active heart rate to make sure your pet has meet their daily activity.
+            -	It will also give alerts to let owners know that they are approaching or have acceded the pet activity for the day and rest is required.
+            -	The monitor will adjust as your pet ages to the different stages in life.
+            -	The monitor will allow you to manage your pet temperatures during all periods.
+-	The monitor will give acceptable portions of food and feeding times to insure your pet is intaking the proper portions for their breed.
+    Animals are complex creatures, and people spend thousands of dollars each year maintaining their pets. This application will allow pet owners to get a true understanding of their breed and what it takes to make sure they can meet their life expectancies, without the high vet bills, and the complex research.
+
+
+
+
+
+}
